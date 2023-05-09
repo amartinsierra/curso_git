0b3d9c5991aa4703fb7cdf99a715d16f06d1f761
Antonio Martín <ajms66@gmail.com>
2023-05-09 11:41:18 +0200

    c1

---
+++ src/service/CalculadoraService.java
@@ -0,0 +1,31 @@
+package service;
+
+import util.Utilidades;
+
+public class CalculadoraService {
+	public int sumar(int a, int b) {
+		return a+b;
+	}
+	public int restar(int a, int b) {
+		return Utilidades.mayor(a, b)-Utilidades.min(a, b);
+	}
+	public int multiplicar(int a, int b) {
+		return a*b;
+	}
+	public int dividir(int a, int b) {
+		return Utilidades.mayor(a, b)/Utilidades.min(a, b);
+	}
+	public int potencia(int a, int b) {
+		return (int)Math.pow(a, b);
+	}
+	public int sumatorio(int[] nums) {
+		int suma=0;
+		for(int n:nums) {
+			suma+=n;
+		}
+		return suma;
+	}
+	public int factorial(int n) {
+		return n*2;
+	}
+}
---
+++ src/util/Utilidades.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Utilidades {
+	public static int mayor(int x, int y) {
+		return Math.max(x, y);
+	}
+	public static int min(int x, int y) {
+		return Math.min(x, y);
+	}
+}
